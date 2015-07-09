0c03a5b0b6c74dda90b9255f2f4b3ac4e3c6cf00
ctsou <ctsou@mitre.org>
2015-07-09 15:34:34 -0400

    initial commit

---
+++ app/src/main/java/kirin/cloud9/MenuActivity.java
@@ -0,0 +1,20 @@
+package kirin.cloud9;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by ctsou on 7/9/2015.
+ */
+public class MenuActivity extends Activity {
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.menu);
+
+    }
+
+
+}
---
+++ app/src/main/java/kirin/cloud9/SplashActivity.java
@@ -0,0 +1,44 @@
+package kirin.cloud9;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.media.MediaPlayer;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class SplashActivity extends Activity {
+
+    static boolean load = false;
+    private MediaPlayer logoMusic;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        setContentView(R.layout.splash);
+
+        //logoMusic = MediaPlayer.create(SplashActivity.this, R.raw.splash_sound);
+        //logoMusic.start();
+        Thread logoTimer = new Thread() {
+            public void run() {
+                try {
+                    sleep(2350);
+                    Intent menuIntent = new Intent(getApplicationContext(), MenuActivity.class);
+                    //logoMusic.stop();
+                    startActivity(menuIntent);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    finish();
+                }
+            }
+        };
+        logoTimer.start();
+    }
+}
+
